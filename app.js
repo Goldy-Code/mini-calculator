ed1093b4aa563b126df9c5f1cb4000d34c4f8dac
Goldy-Code <113247980+Goldy-Code@users.noreply.github.com>
2022-11-12 22:22:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let a = +prompt('num1', '');
+let b = +prompt('num2', '');
+let sum = +(a + b);
+let raz = +(a - b);
+let delen = +(a / b);
+let umnoj = +(a * b);
+console.log(`
+${a} + ${b} = ${sum}
+${a} - ${b} = ${raz}
+${a} / ${b} = ${delen}
+${a} * ${b} = ${umnoj}
+`);
+alert('Check the console log)))')
